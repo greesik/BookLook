09f2229aeba187742c25825433d52a58ec7505a6
Grzegorz Sikora <greesik@gmail.com>
2020-11-06 13:25:35 +0100

    conditions added; app does not crash anymore

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -2,12 +2,17 @@ import React from 'react'
 import "./Book.scss";
 
 const Book = ({volumeInfo}) => {
+
+    let image = (volumeInfo.imageLinks && volumeInfo.imageLinks.smallThumbnail) ? volumeInfo.imageLinks.smallThumbnail : '';
+
     return (
+        volumeInfo ?
         <div className="book-wrapper">
-            <div className="img-wrapper" style={{background:"url("+volumeInfo.imageLinks.smallThumbnail+") no-repeat", backgroundSize:"contain", backgroundPosition:"center"}}></div>
+            <div className="img-wrapper" style={{background:`url(${image}) no-repeat`, backgroundSize:"contain", backgroundPosition:"center"}}></div>
             <div className={"book-basic-details-title"}>{volumeInfo.title}</div>
             <div className={"book-basic-details"}>{volumeInfo.description}</div>
-        </div>
+            {/* <img src={volumeInfo.imageLinks.thumbnail} alt=""/> */}
+        </div> : <div></div>
     )
 }
 
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,7 +4,7 @@ import Book from '../Book/Book';
 
 const Search = () => {
 
-    const [title, setTitle] = useState("python");
+    const [title, setTitle] = useState("");
 
     const handleTitle = (event) => {
         setTitle(event.target.value);
@@ -12,37 +12,39 @@ const Search = () => {
 
     const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        fetchBooks()
-    }, [title]);
-
-
     const fetchBooks = (bookTitle) => {
         bookTitle = title;
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${bookTitle}`)
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`)
             .then(resp => resp.json())
             .then(allBooks => setBooks(allBooks.items))
     };
+    
+    useEffect(() => {
+        fetchBooks();
+    }, [title]);
 
     useEffect(() => {
         console.log(books);
     }, [books])
     
-    
-
     return (
         <section id="search-section">
             <div className="search-header">
                 <h2>Wpisz tytuł, którego szukasz</h2>
                 <form action="">
-                    <input className="search-input" type="text" value={title.toUpperCase()} onChange={handleTitle} onSubmit={handleTitle}/>
+                    <input 
+                    className="search-input" 
+                    type="text" 
+                    value={title.toUpperCase()} 
+                    onChange={handleTitle} 
+                    />
                 </form>
             </div>
             <div className="books-container">
-                {books.map(book =>
+                {books ? books.map(book =>
                 <div key={book.id} className={"book"}>
                     <Book key={book.id} {...book}/>
-                </div>)}
+                </div>) : <div>Brak książek o podanym tytule</div>}
             </div>    
         </section>
     )
